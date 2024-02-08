9c9a7259ef4c029ef8146f4764f1939e779c6ca9
somebody else <somebody.else@example.com>
2024-02-08 15:40:49 +0000

    Change message

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int lines_merged_by_formatting_change() { return 777; }
 
 void lines_split_by_formatting_change() {
-    std::cerr << "Hello, World!\n";
+    std::cerr << "Hello, world!\n";
 }
 
 int main() {
