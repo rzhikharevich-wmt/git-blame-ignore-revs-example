84775a0b2588b0450b5099efda03962fa1fdf4db
automatic formatting <not.roman.zhikharevich@example.com>
2024-02-08 15:38:10 +0000

    Fix formatting

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int lines_merged_by_formatting_change() {
-    return 777;
-}
+int lines_merged_by_formatting_change() { return 777; }
 
-void lines_split_by_formatting_change() { std::cerr << "Hello, World!\n"; }
+void lines_split_by_formatting_change() {
+    std::cerr << "Hello, World!\n";
+}
 
 int main() {
     lines_merged_by_formatting_change();
